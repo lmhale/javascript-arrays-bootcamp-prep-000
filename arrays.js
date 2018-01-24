7a8f373f774c66c110a95b6fcfca312a67c4ea35
Lauren M Hale <lmhale710@gmail.com>
2018-01-24 15:40:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,10 @@
-var chocolateBars = ['snickers', 'hundred grand','kitkat','skittles'];
-
-
 var colors = ['red','blue','green'];
 
 function addElementToBeginningOfArray(array,element) {
 
  colors.unshift(element);
  return colors;
-
+  
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element){
