1070b1c6b5906559917ebc2f26b5ed74a4a91261
Lauren M Hale <lmhale710@gmail.com>
2018-01-24 16:24:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,13 +6,10 @@ var colors = ['red','blue','green','pink','purple'];
 function addElementToBeginningOfArray(array,element){
   array.unshift(element);
   return array;
-  
+
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element){
   return [element,...array];
-  
+
 }
-
-
-
