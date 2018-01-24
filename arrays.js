ee727ad71d024a55a987e9367822bbb8579fb6ea
Lauren M Hale <lmhale710@gmail.com>
2018-01-24 16:33:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,7 @@ function destructivelyAddElementToEndOfArray(array,element) {
 function accessElementInArray(array,index) {
   return array[index];
 }
+function destructivelyRemoveElementFromBeginningOfArray(array){
+   array.shift();
+   return array;
+}
